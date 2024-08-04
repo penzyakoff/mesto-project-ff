9aae7b51b170c686ce0d66ee77ffc6a7ba87c781
Gregory Penzyakov <penzyakov@phystech.edu>
2024-08-04 14:19:06 +0300

    mistake with images

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,19 +1,27 @@
+import '../pages/index.css';
+import { initialCards } from './cards';
+
 const container = document.querySelector(".page");
 const placesList = container.querySelector(".places__list");
-
 const cardTemplate = document.querySelector("#card-template").content;
 
 function addCard(element, deleteCard) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const deleteButton = cardElement.querySelector(".card__delete-button");
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
 
-  cardElement.querySelector(".card__image").src = element.link;
-  cardElement.querySelector(".card__image").alt = element.name;
-  cardElement.querySelector(".card__title").textContent = element.name;
+  cardImage.src = element.link;
+  cardImage.alt = element.name;
+  cardTitle.textContent = element.name;
 
   deleteButton.addEventListener("click", () => deleteCard(cardElement));
 
   return cardElement;
+}
+
+function openCard() {
+  
 }
 
 function deleteCard(card) {
