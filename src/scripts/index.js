c5d0ffe6a1765cb76dcc83c00f907018388a680a
Gregory Penzyakov <penzyakov@phystech.edu>
2024-08-09 01:46:31 +0300

    close and open popup

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -31,3 +31,34 @@ function deleteCard(card) {
 initialCards.forEach((elem) => {
   placesList.append(addCard(elem, deleteCard));
 });
+
+const profileEditPopup = document.querySelector('.popup_type_edit');
+const profileEditButton = document.querySelector('.profile__edit-button');
+const profileAddPopup = document.querySelector('.popup_type_new-card');
+const profileAddButton = document.querySelector('.profile__add-button');
+const popupCloseButton = document.querySelectorAll('.popup_close');
+
+const handleCloseOnEsc = (evt) => {
+  if (evt.key === 'Escape') {
+    const openPopup = document.querySelector('.popup_is-opened');
+    closePopup(openPopup)
+  }
+}
+
+const openPopup = (popup) => {
+  popup.classList.add('popup_is-opened');
+  document.addEventListener('keydown', handleCloseOnEsc)
+}
+
+const closePopup = (popup) => {
+  popup.classList.remove('popup_is-opened');
+  document.removeEventListener('keydown', handleCloseOnEsc)
+}
+
+//profileEditButton.addEventListener('click', () => openPopup(profileEditPopup));
+//profileAddButton.addEventListener('click', () => openPopup(profileAddPopup));
+
+popupCloseButton.forEach(event => {
+  const popup = event.closest('.popup');
+  event.addEventListener('click', () => closePopup(popup))
+})
